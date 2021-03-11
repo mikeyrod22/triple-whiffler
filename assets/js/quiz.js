6d4135413ff40a65e9d7b86e962cc2e109dcdb56
Michael Rodriguez <mikeyrod22@gmail.com>
2021-03-11 17:02:18 -0600

    wrote two question objects, got functionality of displayQuestionCard() mostly where I want it for now, added small amounts of styling.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,14 +1,62 @@
 
-let questOne = {question:"2+2", correct:"4", incorrectOne: "1", incorrectTwo:"2", incorrectThree:"3"};
-let questTwo = {question:"2+2", correct:"4", incorrectOne: "1", incorrectTwo:"2", incorrectThree:"3"};
-let questThree = {question:"2+2", correct:"4", incorrectOne: "1", incorrectTwo:"2", incorrectThree:"3"};
-let questFour = {question:"2+2", correct:"4", incorrectOne: "1", incorrectTwo:"2", incorrectThree:"3"};
-let questFive = {question:"2+2", correct:"4", incorrectOne: "1", incorrectTwo:"2", incorrectThree:"3"};
-let questSix = {question:"2+2", correct:"4", incorrectOne: "1", incorrectTwo:"2", incorrectThree:"3"};
-let questSeven = {question:"2+2", correct:"4", incorrectOne: "1", incorrectTwo:"2", incorrectThree:"3"};
-let questEight = {question:"2+2", correct:"4", incorrectOne: "1", incorrectTwo:"2", incorrectThree:"3"};
+let questOne = {
+    question:"What symbol is used to select elements by class in CSS?", 
+    correct:".", 
+    incorrectOne: "#", 
+    incorrectTwo:"*", 
+    incorrectThree:"$"
+};
+let questTwo = {
+    question:"What data type is captured when a user responds to the window.prompt() method?", 
+    correct:"string", 
+    incorrectOne: "number", 
+    incorrectTwo:"boolean", 
+    incorrectThree:"array"
+};
+let questThree = {
+    question:"2+2", 
+    correct:"4", 
+    incorrectOne: "1", 
+    incorrectTwo:"2", 
+    incorrectThree:"3"
+};
+let questFour = {
+    question:"2+2", 
+    correct:"4", 
+    incorrectOne: "1", 
+    incorrectTwo:"2", 
+    incorrectThree:"3"
+};
+let questFive = {
+    question:"2+2", 
+    correct:"4", 
+    incorrectOne: "1", 
+    incorrectTwo:"2", 
+    incorrectThree:"3"
+};
+let questSix = {
+    question:"2+2", 
+    correct:"4", 
+    incorrectOne: "1", 
+    incorrectTwo:"2", 
+    incorrectThree:"3"
+};
+let questSeven = {
+    question:"2+2", 
+    correct:"4", 
+    incorrectOne: "1", 
+    incorrectTwo:"2", 
+    incorrectThree:"3"
+};
+let questEight = {
+    question:"2+2", 
+    correct:"4", 
+    incorrectOne: "1", 
+    incorrectTwo:"2", 
+    incorrectThree:"3"
+};
 
-let questionsArray = [questOne, questTwo, questThree, questFour, questFive, questSix, questSeven, questEight,];
+let questionObjectsArray = [questOne, questTwo, questThree, questFour, questFive, questSix, questSeven, questEight,];
 
 let timeRemainingEl = document.querySelector('#time-remaining');
 let timeRemaining;
@@ -16,7 +64,7 @@ let timeRemaining;
 function startTimer() {
     timeRemaining = 100;
     timeRemainingEl.innerHTML = `Time Remaining: ${timeRemaining}`;
-    console.log(`Time Remaining: ${timeRemaining}`);
+    // console.log(`Time Remaining: ${timeRemaining}`);
     let timerSetInterval = setInterval(function(){
         if (timeRemaining < 1) {
             timeRemaining = 0;
@@ -26,17 +74,31 @@ function startTimer() {
         }
         timeRemaining--;
         timeRemainingEl.innerHTML = `Time Remaining: ${timeRemaining}`;
-        console.log(`Time Remaining: ${timeRemaining}`);
+        // console.log(`Time Remaining: ${timeRemaining}`);
     }, 1000);
 } 
 
-function displayQuestion() {
-    let questionCard = document.createElement("div");
-    let questionString = document.createTextNode(questOne.question);
-    questionCard.className = "question-card";
-    questionCard.appendChild(questionString);
-    document.body.appendChild(questionCard);
+let mainEl = document.querySelector("#main");
+let questionCardEl = document.createElement("div");
+let questionCardH2 = document.createElement("h2");
+
+function displayQuestionCard(selectedQuestion) {
+    if (mainEl.contains(questionCardEl)) {
+        questionCardEl.removeChild(questionCardH2)
+        questionCardEl.remove();
+    } 
+    questionCardEl.className = "question-card";
+    questionCardH2.innerText = selectedQuestion.question;
+    questionCardEl.appendChild(questionCardH2);
+    mainEl.appendChild(questionCardEl);
 }
+
+function beginQuiz() {
+    var i = 0;
+    startTimer();
+    displayQuestionCard(questionObjectsArray[i])
+    }
+
 
 // AS A coding boot camp student
 // I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
